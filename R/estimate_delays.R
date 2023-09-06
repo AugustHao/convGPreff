d8afd4b018202a63a30d00266596fca7e56620af
AugustHao <tianxiao.hao@telethonkids.org.au>
2023-09-06 00:41:01 +0000

    changed documentation of the dummy delay function

--- R/estimate_delays.R
+++ R/estimate_delays.R
@@ -1,14 +1,11 @@
-#' Takes a data frame of "ascertainment_delay_data", and returns a delay ecdf across all time or a
-#' list of ecdfs for each time step. The ascertainment_delay_data should have a column of days (or
-#' other time intervals) and a column of FORWARD delays from the corresponding days. The name of
-#' this input parameter implies delays are to do with the ascertainment process, i.e. from onset to
-#' notification. Howeever this function can be used for other types of delays, such as from
-#' infection to onset, should such data be available in a table format. In reality the ecdf for the
-#' incubation period is likely to be inferred from some point and variance estimates from the
-#' literature, not from linelists, so its estimation is unlikely to use this function. However, this
-#' function might be useful for linelisted systematic delays in the notification system, e.g. the
-#' delay from notification in some interim database to notification in the final dataset --- we need
-#' to keep this use case in mind.
+#' Takes a data frame of "notification_delay_data", and returns a delay ecdf
+#' across all time or a list of ecdfs for each time step. The
+#' notification_delay_data should have a column of days (or other time
+#' intervals) and a column of FORWARD delays from the corresponding days. This
+#' is a fudge function to quickly put together delay data in the format usable
+#' for the rest of the code, the real delay data will have more missingness each
+#' day, and therefore need to be estimated with a rolling time window, in a more
+#' complicated way
 #'
 #' @param notification_delay_data
 #' @param time_varying
