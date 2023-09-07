9d0b77fbf753ec10d20e9cd5a0042272ad7b219e
AugustHao <tianxiao.hao@telethonkids.org.au>
2023-09-07 05:33:48 +0000

    dimensionality tweak for infection vs obs timeseries

--- tests/tests_manual.R
+++ tests/tests_manual.R
@@ -15,22 +15,27 @@ library(greta.dynamics)
 
 #load real case count data if using
 #this is not pushed up because of data sharing permission barriers
-#reff_data <- readRDS("tests/reff_data_2023_08_17.rds")
+reff_data <- readRDS("tests/reff_data_2023_08_17.rds")
 
-#local_cases <- reff_data$local$cases
+obs_N <- reff_data$local$cases
 
-#n_jurisdictions <- ncol(local_cases)
-#n_days <- nrow(local_cases)
-
-#specifying infection matrix dim for simulated data
-n_jurisdictions <- 4
-n_days <- 101
+n_jurisdictions <- ncol(obs_N)
+n_days <- nrow(obs_N)
+#
+# #specifying infection matrix dim for simulated data
+# n_jurisdictions <- 4
+# n_days <- 101
 days <- seq_len(n_days)
 
-# load a set of simulated data that had known convergence issues
-obs_N <- readRDS("tests/sim_case_data.RData")
-obs_N <- rbind(rep(0,4), obs_N)
 
+#start infection timeseries a few days before notification
+n_days_infection <- n_days + 3
+days_infection <- seq_len(n_days_infection)
+
+# # # load a set of simulated data that had known convergence issues
+# obs_N <- readRDS("tests/sim_case_data.RData")
+# obs_N <- rbind(rep(0,4), obs_N)
+#
 # #alternatively, simulate data using the following true infection curve
 # f_true <- function(t) exp(-1 + 3 * (sin(t / 5) + sin(t / 10)))
 # f_true <- function(t) exp(2 + 2 * sin(t / 5 - 2))
@@ -59,8 +64,8 @@ obs_N <- rbind(rep(0,4), obs_N)
 set.seed(2023-06-26)
 
 notification_delay_data <- tibble(
-    days = sample(days,n_days*100,replace = T),
-    delay = ceiling(rnorm(n_days*100,mean = 3,sd = 0.5))
+    days = sample(days_infection,n_days_infection*100,replace = T),
+    delay = ceiling(rnorm(n_days_infection*100,mean = 3,sd = 0.5)) #rep(3,n_days*100)
 )
 
 # estimate a time-varying function (returning a vector of delay functions)
@@ -68,7 +73,7 @@ notification_delay_data <- tibble(
 source("R/estimate_delays.R")
 notification_delay_function_raw <- estimate_delays(
     notification_delay_data = notification_delay_data,
-    time_varying = FALSE #currently testing if time-fixed delay help with convergence
+    time_varying = TRUE #currently testing if time-fixed delay help with convergence
 )
 
 #quick check delay behaviour
@@ -106,9 +111,12 @@ car <- 1
 #gp specifications for infection matrix
 
 
-# lengthscale prior
-gp_lengthscale <- lognormal(0, 1)
+#lengthscale inverse gamma prior algebraically calculated alpha and beta based
+#on assumed lower and upper bounds of 2,11
 
+# posterior lengthscale tends to higher value, exceeding the upperbound for
+# inverse gamma specified, intersting, need to look into this further
+gp_lengthscale <- lognormal(0, 1) #inverse_gamma(187/9,1157/18)
 #test prior behaviour for lengthscale - this is a known problem hyperparameter
 #which sometimes tends to extreme small or large values. Currently looking into
 #inverse gamma prior for more stability
@@ -123,7 +131,7 @@ fixed_kernel <- mat52(1, 1)
 
 #define gp
 gp <- greta.gp::gp(
-    x = days,
+    x = days_infection,
     kernel = gp_kernel,
     n = n_jurisdictions,
     tol = 1e-3
@@ -146,11 +154,12 @@ infections <- compute_infections(
 # do convolutions
 source("R/convolve.R")
 source("R/get_convolution_matrix.R")
+#debugonce(convolve)
 expected_cases <- convolve(timeseries_matrix = infections,
-                           mass_functions = test_full_delay[[1]],
+                           mass_functions = test_full_delay,
                            proportion = car)
 
-calculate(expected_cases,nsim = 1)
+calculate(expected_cases, nsim = 1)
 
 # define observation models
 # here are a couple of alternative distribution choices tried
@@ -165,7 +174,7 @@ sqrt_inv_size <- normal(0,
                         0.5,
                         truncation = c(0, Inf),
                         dim = n_jurisdictions)
-sqrt_inv_size <- sweep(zeros(n_days,n_jurisdictions),
+sqrt_inv_size <- sweep(zeros(n_days_infection,n_jurisdictions),
                        2,
                        sqrt_inv_size,
                        FUN = "+")
@@ -176,8 +185,13 @@ size <- 1 / sqrt(sqrt_inv_size)
 #calculate(size,nsim = 1)
 prob <- 1 / (1 + expected_cases / size)
 
-distribution(obs_N) <- negative_binomial(size,
-                                         prob)
+
+size_obs <- size[(3+1):(3+n_days),]
+
+prob_obs <- prob[(3+1):(3+n_days),]
+#take out the extra infection days
+distribution(obs_N) <- negative_binomial(size_obs,
+                                         prob_obs)
 
 m <- model(infections,
            expected_cases,
@@ -205,7 +219,7 @@ draws <- mcmc(m,warmup = 1000,
 coda::gelman.diag(draws, autoburnin = FALSE, multivariate = FALSE)$psrf[, 1]
 
 
-case_sims <- calculate(expected_cases,
+case_sims <- calculate(expected_cases[(3+1):(3+n_days),],
                        values = draws,
                        nsim = 1000)
 
@@ -217,6 +231,12 @@ source("R/plot_posterior_timeseries_with_data.R")
 plot_posterior_timeseries_with_data(simulations = case_sims$expected_cases,
                                     data_mat = obs_N)
 
+infections_sims <- calculate(infections[(3+1):(3+n_days),],
+                       values = draws,
+                       nsim = 1000)
+
+plot_posterior_timeseries_with_data(simulations = infections_sims$infections,
+                                    data_mat = obs_N)
 
 #reset test condition
 rm(gp,
@@ -249,14 +269,14 @@ rm(gp,
 #
 # View(infections_sim)
 #
-# gp_lengthscale_sim <- calculate(gp_lengthscale,
-#                                 values = draws,
-#                                 nsim = 100)
+gp_lengthscale_sim <- calculate(gp_lengthscale,
+                                values = draws,
+                                nsim = 100)
+
+gp_lengthscale_sim <- apply(gp_lengthscale_sim[[1]], 2:3, FUN = "mean")
 #
-# gp_lengthscale_sim <- apply(gp_lengthscale_sim[[1]], 2:3, FUN = "mean")
-#
-# gp_variance_sim <- calculate(gp_variance,
-#                              values = draws,
-#                              nsim = 100)
-#
-# gp_variance_sim <- apply(gp_variance_sim[[1]], 2:3, FUN = "mean")
+gp_variance_sim <- calculate(gp_variance,
+                             values = draws,
+                             nsim = 100)
+
+gp_variance_sim <- apply(gp_variance_sim[[1]], 2:3, FUN = "mean")
