f29ff016e2f0c9b01e4f0f3bf23bef195c2534fb
AugustHao <tianxiao.hao@telethonkids.org.au>
2023-09-26 02:10:38 +0000

    functionality to plot posterior timeseries samples from fitted model as ribbon plots

---
+++ R/colours.R
@@ -0,0 +1,10 @@
+# colours for plotting
+blue <- "steelblue3"
+purple <- "#C3A0E8"
+green <- RColorBrewer::brewer.pal(8, "Set2")[1]
+yellow <- RColorBrewer::brewer.pal(8, "Set2")[6]
+blue_green <- colorRampPalette(c("blue", green))(10)[8]
+yellow_green <- colorRampPalette(c("yellow", green))(10)[8]
+orange <- RColorBrewer::brewer.pal(8, "Set2")[2]
+pink <- RColorBrewer::brewer.pal(8, "Set2")[4]
+fifo <- "#A8EB12"
